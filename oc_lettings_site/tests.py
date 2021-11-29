4d00bbe1ecf6176c0c146df31ebccecd5c9f3eca
Virgin Bitton <virgin.bitton@gmail.com>
2021-11-29 21:31:35 +0100

    mise à jour du premier test

--- oc_lettings_site/tests.py
+++ oc_lettings_site/tests.py
@@ -1,2 +1,15 @@
-def test_dummy():
-    assert 1
+import pytest
+
+from django.urls import reverse
+
+
+@pytest.mark.django_db
+def test_main_index(client):
+    url = reverse('index')
+    response = client.get(url)
+
+    html_content = str(response.content)
+    h1_tag = html_content.split('<h1>', 1)[1].split('</h1>', 1)[0]
+
+    assert 'Welcome to Holiday Homes' in h1_tag
+    assert response.status_code == 200

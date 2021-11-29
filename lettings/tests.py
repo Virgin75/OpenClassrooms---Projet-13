f194adead36bc3007e93b72eac4f78bf57961a0b
Virgin Bitton <virgin.bitton@gmail.com>
2021-11-29 21:54:54 +0100

    ajout des tests pour l'app lettings

--- lettings/tests.py
+++ lettings/tests.py
@@ -1,3 +1,32 @@
-from django.test import TestCase
+import pytest
 
-# Create your tests here.
+from django.urls import reverse
+from .models import Letting, Address
+
+
+@pytest.mark.django_db
+def test_letings_index(client):
+    url = reverse('lettings:index')
+    response = client.get(url)
+
+    html_content = str(response.content)
+    h1_tag = html_content.split('<h1>', 1)[1].split('</h1>', 1)[0]
+
+    assert 'Lettings' in h1_tag
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_letings_view(client):
+    address = Address.objects.create(number=21, street='Rue du Gonfe', city='Paris',
+                                     state='IDF', zip_code=75001, country_iso_code='FRA')
+    letting = Letting.objects.create(title='Nouvelle Location Bordeaux', address=address)
+
+    url = reverse('lettings:letting', kwargs={'letting_id': letting.id})
+    response = client.get(url)
+
+    html_content = str(response.content)
+    h1_tag = html_content.split('<h1>', 1)[1].split('</h1>', 1)[0]
+
+    assert letting.title in h1_tag
+    assert response.status_code == 200

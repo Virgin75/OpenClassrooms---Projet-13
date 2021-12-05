7d4d6c457b4d17e63e1e00ecf3c941edd3096923
Virgin Bitton <virgin.bitton@gmail.com>
2021-12-05 12:45:20 +0100

    correction des dernières erreurs flake8

--- profiles/views.py
+++ profiles/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .models import Profile
 
 
-'''Sed placerat quam in pulvinar commodo. Nullam laoreet consectetur ex, sed consequat 
+'''Sed placerat quam in pulvinar commodo. Nullam laoreet consectetur ex, sed consequat
 libero pulvinar eget. Fusc faucibus, urna quis auctor pharetra, massa dolor cursus
 neque, quis dictum lacus d'''
 
@@ -13,9 +13,9 @@ def index(request):
     return render(request, 'profiles/index.html', context)
 
 
-'''Aliquam sed metus eget nisi tincidunt ornare accumsan eget lac laoreet neque quis, 
-pellentesque dui. Nullam facilisis pharetra vulputate. Sed tincidunt, dolor id facilisis 
-fringilla, eros leo tristique lacus, it. Nam aliquam dignissim congue. Pellentesque 
+'''Aliquam sed metus eget nisi tincidunt ornare accumsan eget lac laoreet neque quis,
+pellentesque dui. Nullam facilisis pharetra vulputate. Sed tincidunt, dolor id facilisis
+fringilla, eros leo tristique lacus, it. Nam aliquam dignissim congue. Pellentesque
 habitant morbi tristique senectus et netus et males'''
 
 

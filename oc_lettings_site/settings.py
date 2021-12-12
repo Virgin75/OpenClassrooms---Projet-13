824b35179e73de058118d5af9c68d4a9752548ed
Virgin Bitton <virgin.bitton@gmail.com>
2021-12-12 17:14:26 +0100

    add sentry sdk to config file

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -1,7 +1,22 @@
 import os
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
 from dotenv import load_dotenv
 load_dotenv()  # loads the configs from
 
+sentry_sdk.init(
+    dsn="https://f085df5dc5c84a1cb1d4526cd5d68bb0@o1089173.ingest.sentry.io/6104080",
+    integrations=[DjangoIntegration()],
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))

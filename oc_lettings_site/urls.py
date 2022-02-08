493a96d6bb91e3cc83c07a35512715c9a535194d
Virgin Bitton <virgin.bitton@gmail.com>
2022-02-08 23:28:15 +0100

    correction des derniers bugs de linting

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
 from . import views
 
 
@@ -16,5 +14,4 @@ urlpatterns = [
     path('profiles/', include('profiles.urls', namespace='profiles')),
     path('admin/', admin.site.urls),
     path('sentry-debug/', trigger_error),
-    
 ]
